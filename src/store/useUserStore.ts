1f2915237d50ad77031a92c412f13ffbffec536b
rachel <rachel.uiux@gmail.com>
2023-12-18 21:34:40 +0900

    feat : IDE-75 유저 정보 표시 테스트

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,19 +1,38 @@
 import { create } from 'zustand';
 
+// 랜덤 라이브커서 색 생성
+const hex = '#' + Math.round(Math.random() * 0xffffff).toString(16);
+
+// 랜덤 이름 생성
+const names = ['Dehan', 'Musfiq', 'Rahim', 'Sohel', 'MOhit', 'Rachel'];
+const randInt = Math.floor(Math.random() * names.length);
+const name = names[randInt];
+
+// 랜덤 ID 생성 -> 실제 유저 데이터로 바꿀 예정
+const randId = Math.floor(Math.random() * 100);
+
 interface UserState {
-  id: string | null;
-  name: string | null;
+  id: number | null;
+  name: string;
   imageUrl: string | null;
-  setUser: (id: string, name: string, imageUrl: string) => void;
+  cursorColor: string;
+  setUser: (
+    id: number,
+    name: string,
+    imageUrl: string,
+    cursorColor: string
+  ) => void;
   isLoggedIn: boolean;
   toggleLogin: () => void;
 }
 
 const useUserStore = create<UserState>(set => ({
-  id: null,
-  name: null,
+  id: randId,
+  name: name,
   imageUrl: null,
-  setUser: (id, name, imageUrl) => set({ id, name, imageUrl }),
+  cursorColor: hex,
+  setUser: (id, name, imageUrl, cursorColor) =>
+    set({ id, name, imageUrl, cursorColor }),
   isLoggedIn: false,
   toggleLogin: () => set(state => ({ isLoggedIn: !state.isLoggedIn })),
 }));
