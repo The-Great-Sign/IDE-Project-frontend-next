5867cffe75dc3569514fcfc215961d5abea180e1
nari <naringst@gmail.com>
2023-12-19 02:00:08 +0900

    feat : IDE-78 현재 파일의 전체 경로 찾는 함수 구현

---
+++ src/utils/fileTreeUtils.ts
@@ -0,0 +1,14 @@
+import { NodeData } from '@/types/IDE/FileTree/FileDataTypes';
+import { NodeApi } from 'react-arborist';
+
+export const findNowFilePath = (node: NodeApi<NodeData> | null) => {
+  let filePath = '';
+  while (node!.data.id !== '__REACT_ARBORIST_INTERNAL_ROOT__') {
+    const newFile = '/' + node!.data.name;
+    filePath = newFile + filePath;
+    if (node) {
+      node = node.parent;
+    }
+  }
+  return filePath;
+};
