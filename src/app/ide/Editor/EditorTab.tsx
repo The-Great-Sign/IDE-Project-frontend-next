7d17ec1607cd1d0063549faa51bb96f5f97eef66
rachel <rachel.uiux@gmail.com>
2023-12-20 22:59:57 +0900

    style : IDE-75 fileTab 스타일 임시 반영 및 분리, IDE 헤더 영역 분리
    - 활성화된 탭 표시 필요

--- src/app/ide/Editor/EditorTab.tsx
+++ src/app/ide/Editor/EditorTab.tsx
@@ -1,61 +1,33 @@
 import React from 'react';
 import { useFileStore } from '@/store/useFileStore';
-import styled from 'styled-components';
-import { AiOutlineClose } from 'react-icons/ai';
 
-// Styled components for tabs
-const TabsContainer = styled.div`
-  display: flex;
-  background: #333;
-  padding: 0.5rem;
-`;
-
-const Tab = styled.div<{ isSelected: boolean }>`
-  margin-right: 0.5rem;
-  padding: 0.5rem 1rem;
-  background: ${props =>
-    props.isSelected ? '#555' : '#444'}; // Conditional styling
-  color: white;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  &:hover {
-    background-color: #555;
-  }
-`;
-
-const TabTitle = styled.span`
-  margin-right: 10px;
-`;
-
-const CloseIcon = styled(AiOutlineClose)`
-  color: white;
-  cursor: pointer;
-`;
+import {
+  CloseIcon,
+  EditorTabContainer,
+  FileClose,
+  FileInfo,
+  FileTab,
+} from './EditorTab.styles';
 
 const EditorTab = () => {
-  const { files, selectedFileId, selectFile, closeFile } = useFileStore();
+  const { files, selectFile, closeFile } = useFileStore();
 
   return (
-    <TabsContainer>
+    <EditorTabContainer>
       {files.map(file => (
-        <Tab
-          key={file.id}
-          isSelected={file.id === selectedFileId}
-          onClick={() => selectFile(file.id)}
-        >
-          <TabTitle>{file.name}</TabTitle>
-          <CloseIcon
+        <FileTab key={file.id} onClick={() => selectFile(file.id)}>
+          <FileInfo>{file.name}</FileInfo>
+          <FileClose
             onClick={e => {
               e.stopPropagation(); // Prevents triggering selectFile when closing
               closeFile(file.id);
             }}
-          />
-        </Tab>
+          >
+            <CloseIcon />
+          </FileClose>
+        </FileTab>
       ))}
-    </TabsContainer>
+    </EditorTabContainer>
   );
 };
 
