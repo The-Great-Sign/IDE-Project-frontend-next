025e431737a8c7172d9c79188fd355f243a514b7
nari <naringst@gmail.com>
2023-12-20 21:32:39 +0900

    feat : IDE-106 파일 생성, 파일 열기 서버 연결 확인

--- src/app/ide/FileTree/Node.tsx
+++ src/app/ide/FileTree/Node.tsx
@@ -19,23 +19,61 @@ export const Node = ({
 }: NodeRendererProps<FileNodeType>) => {
   const { updateNodeName } = useFileTreeStore();
   const { setOpenFilesIdList } = useCurrentOpenFileList();
+  const projectId = 'ebc63279-89b9-4b1d-bb4d-1270130c3d4d'; //임시
 
   const handleOpenFile = async () => {
     try {
       const nowFilePath = findNowFilePath(node);
-      const projectId = 'tempProjectId';
+      const params = { projectId: projectId, filePath: nowFilePath };
 
-      const { data } = await axiosInstance.post('/api/files', {
-        projectId: projectId,
-        filePath: nowFilePath,
+      const { data } = await axiosInstance.get('/api/files', {
+        params: params,
       });
 
       //응답받은 filename, content .. 등 필요 정보 담아두기 -> 총미
 
       //열린 파일 목록 업데이트 -> 총미
       setOpenFilesIdList(node.id);
-
+      console.log(data);
       return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCreateFileRequest = async (newNodeName: string) => {
+    try {
+      const nowFilePath = findNowFilePath(node) + newNodeName;
+      let responseData;
+
+      //directory or file 구별
+      if (node.isInternal) {
+        const response = await axiosInstance.post('/api/files', {
+          projectId: projectId,
+          directories: nowFilePath,
+          files: null,
+          content: 'print("Hello, World!")',
+        });
+        responseData = response.data;
+      } else {
+        const response = await axiosInstance.post('/api/files', {
+          projectId: projectId,
+          directories: null,
+          files: nowFilePath,
+          content: 'print("Hello, World!")',
+        });
+        responseData = response.data;
+      }
+
+      console.log(projectId);
+      console.log(nowFilePath);
+
+      //응답받은 filename, content .. 등 필요 정보 담아두기 -> 총미
+
+      //열린 파일 목록 업데이트 -> 총미
+      setOpenFilesIdList(node.id);
+      console.log(responseData);
+      return responseData;
     } catch (error) {
       console.error(error);
     }
@@ -91,6 +129,7 @@ export const Node = ({
                 if (e.key === 'Escape') node.reset();
                 if (e.key === 'Enter') {
                   //이때 서버로도 메시지 보내야 함. 성공시 아래 코드 실행
+                  handleCreateFileRequest(e.currentTarget.value);
                   updateNodeName(node.id, e.currentTarget.value);
                   node.submit(e.currentTarget.value);
                 }
