ec816e70bf383e06db61440458ba3cfa597fc9ff
rachel <rachel.uiux@gmail.com>
2023-12-22 01:09:46 +0900

    feat : IDE-115 다크/라이트 테마 구현, next.js 깜박임 문제 해결
    - install : @swc/plugin-styled-components, .swcrc 파일 추가
    - docs : next.config.js styledComponents 설정
    - fix : 스타일 적용 안된 html 먼저 보이는 문제 해결
    - window, localstorage 인식 문제 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import MainHeader from '@/components/MainHeader/MainHeader';
 import { MainContainer } from './page.styles';
+// import ThemeToggleBtn from '@/components/ThemeToggleBtn/ThemeToggleBtn';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <MainHeader />
       <MainContainer>
         <h2>대박징조 DJIDE PROJECT</h2>
+        {/* <ThemeToggleBtn /> */}
       </MainContainer>
     </>
   );
