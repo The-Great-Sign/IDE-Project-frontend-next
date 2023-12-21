59f43d45edd838ae8108e31385d07bd43b7d823a
nari <naringst@gmail.com>
2023-12-21 19:46:53 +0900

    fix : IDE-119 DnD 기능에 따른 노드 추가 setter 함수 변경

--- src/store/useFileTreeStore.ts
+++ src/store/useFileTreeStore.ts
@@ -10,70 +10,6 @@ const data: FileNodeType[] = [
     isDirty: false,
     isOpened: true,
     language: 'python',
-    children: [
-      {
-        id: '2',
-        name: 'hello2.py',
-        content: 'print("hello python2")',
-        isDirty: false,
-        isOpened: true,
-        language: 'python',
-      },
-      {
-        id: '3',
-        name: 'hello3.py',
-        content: 'print("hello python3")',
-        isDirty: false,
-        isOpened: true,
-        language: 'python',
-        children: [
-          {
-            id: '4',
-            name: 'hello4.py',
-            content: 'print("hello python4")',
-            isDirty: false,
-            isOpened: true,
-            language: 'python',
-            children: [
-              {
-                id: 'sdfs5',
-                name: 'hello5.py',
-                content: 'print("hello python2")',
-                isDirty: false,
-                isOpened: true,
-                language: 'python',
-              },
-              {
-                id: '32342',
-                name: 'hello6.py',
-                content: 'print("hello python3")',
-                isDirty: false,
-                isOpened: true,
-                language: 'python',
-                children: [
-                  {
-                    id: '4234234',
-                    name: 'hello7.py',
-                    content: 'print("hello python4")',
-                    isDirty: false,
-                    isOpened: true,
-                    language: 'python',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: '5',
-    name: 'hell55o.py',
-    content: 'print("hello55 python")',
-    isDirty: true,
-    isOpened: true,
-    language: 'python',
   },
 ];
 
@@ -82,7 +18,7 @@ interface FileTreeState {
   fileTree: FileNodeType[];
   setFileTree: (fileTree: FileNodeType[]) => void;
   updateNodeName: (nodeId: string, newName: string) => void;
-  addNode: (newNode: FileNodeType, parentId: string | null) => void;
+  addNode: (newNode: FileNodeType, parentId?: string | null) => void;
   deleteNode: (nodeids: string | null) => void;
 }
 
@@ -96,18 +32,24 @@ export const useFileTreeStore = create<FileTreeState>(set => ({
         return node.id === nodeId ? { ...node, name: newName } : node;
       }),
     })),
-  addNode: (newNode: FileNodeType, parentId: string | null) =>
+  addNode: (newNode: FileNodeType, parentId?: string | null) =>
     set(state => {
       const addNodeToTree = (nodes: FileNodeType[]): FileNodeType[] =>
-        nodes.map(node =>
-          node.id === parentId
-            ? { ...node, children: [...(node.children || []), newNode] }
-            : {
-                ...node,
-                children: node.children ? addNodeToTree(node.children) : [],
-              }
-        );
-
+        nodes.map(node => {
+          if (node.id === parentId) {
+            return {
+              ...node,
+              children: [...(node.children || []), newNode],
+            };
+          } else {
+            return {
+              ...node,
+              children: node.children
+                ? addNodeToTree(node.children)
+                : node.children,
+            };
+          }
+        });
       return {
         fileTree: parentId
           ? addNodeToTree(state.fileTree)
