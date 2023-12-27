695e24c317fcaca55f74fbdbcdaef52ebbe3cf0d
rachel <rachel.uiux@gmail.com>
2023-12-27 18:02:20 +0900

    style : IDE-145 InfoBox 공통 컴포넌트화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import MainHeader from '@/components/MainHeader/MainHeader';
-import { MainContainer, MainDiv, MainDivInfo } from './page.styles';
+import { MainContainer, MainDiv } from './page.styles';
 import { BigButton } from '@/components/Button/Button';
 import { useRouter } from 'next/navigation';
 import useUserStore from '@/store/useUserStore';
+import { InfoBox } from '@/components/InfoBox/InfoBox.styles';
 
 const App = () => {
   const router = useRouter();
@@ -29,22 +30,22 @@ const App = () => {
         </h3>
         <div>
           <MainDiv>
-            <MainDivInfo>
-              <h2>채팅기능</h2>
+            <InfoBox>
+              <p>채팅기능</p>
               <div>텍스트 채팅 가능</div>
-            </MainDivInfo>
-            <MainDivInfo>
-              <h2>편리한 코드 실행</h2>
+            </InfoBox>
+            <InfoBox>
+              <p>편리한 코드 실행</p>
               <div>버튼 하나로 실행하는 코드</div>
-            </MainDivInfo>
-            <MainDivInfo>
-              <h2>실시간 동시 편집 가능</h2>
+            </InfoBox>
+            <InfoBox>
+              <p>실시간 동시 편집 가능</p>
               <div>CRDT 활용 실시간 동시 편집</div>
-            </MainDivInfo>
-            <MainDivInfo>
-              <h2>AI에게 질문</h2>
+            </InfoBox>
+            <InfoBox>
+              <p>AI에게 질문</p>
               <div>Chat GPT 활용 간편 코드 리뷰</div>
-            </MainDivInfo>
+            </InfoBox>
           </MainDiv>
         </div>
         <BigButton onClick={handleClick}>시작하기</BigButton>
