50a5b57c533dd64260b26570a83bd95a0f32f9e4
rachel <rachel.uiux@gmail.com>
2023-12-27 14:01:29 +0900

    feat : IDE-75 코드 에디터 저장 버튼 구현

--- src/app/ide/Header/IDEHeader.tsx
+++ src/app/ide/Header/IDEHeader.tsx
@@ -13,8 +13,8 @@ import {
 import { FaPlay } from 'react-icons/fa6';
 import { FaCheckCircle } from 'react-icons/fa';
 import axiosInstance from '@/app/api/axiosInstance';
-// import { useFileStore } from '@/store/useFileStore';
-// import { useFileTreeStore } from '@/store/useFileTreeStore';
+import { useFileStore } from '@/store/useFileStore';
+import { useFileTreeStore } from '@/store/useFileTreeStore';
 import { Client } from '@stomp/stompjs';
 import { getCurrentProjectId } from '../[projectId]/page';
 import { useVisibleChat } from '@/store/useChattingStore';
@@ -25,34 +25,36 @@ interface IDEHeaderProps {
 
 const IDEHeader: React.FC<IDEHeaderProps> = ({ clientRef }) => {
   const { toggleChat } = useVisibleChat();
-  // const { files, selectedFileId } = useFileStore();
-  // const { findNodePath } = useFileTreeStore();
+  const { files, selectedFileId } = useFileStore();
+  const { findNodePath } = useFileTreeStore();
 
-  // [TO DO] 구현 필요
-  // const handleSave = async () => {
-  //   if (selectedFileId) {
-  //     const selectedFile = files.find(f => f.id === selectedFileId);
+  const handleSave = async () => {
+    if (selectedFileId) {
+      const selectedFile = files.find(f => f.id === selectedFileId);
 
-  //     if (selectedFile) {
-  //       const filePath = findNodePath(selectedFileId); // 전체 파일 경로 찾기
-  //       if (filePath) {
-  //         try {
-  //           await axiosInstance.post('/api/v2/files', {
-  //             // fileId 제대로 받아오기
-  //             fileId,
-  //             path: filePath,
-  //             content: selectedFile.content,
-  //           });
-  //           console.log('selectedFile.content: ', selectedFile.content);
-  //           alert('파일이 저장되었습니다.');
-  //         } catch (error) {
-  //           console.error('파일 저장 중 오류 발생:', error);
-  //           alert('파일 저장에 실패했습니다.');
-  //         }
-  //       }
-  //     }
-  //   }
-  // };
+      if (selectedFile) {
+        const filePath = findNodePath(selectedFileId); // 전체 파일 경로 찾기
+        const fileContent = selectedFile.content;
+        console.log(selectedFileId);
+        console.log(typeof selectedFileId);
+        if (filePath) {
+          try {
+            await axiosInstance.put('/api/v2/files', {
+              // fileId 제대로 받아오기
+              fileId: selectedFileId,
+              path: filePath,
+              content: fileContent,
+            });
+            console.log('selectedFile.content: ', selectedFile.content);
+            alert('파일이 저장되었습니다.');
+          } catch (error) {
+            console.error('파일 저장 중 오류 발생:', error);
+            alert('파일 저장에 실패했습니다.');
+          }
+        }
+      }
+    }
+  };
 
   const handleClose = async () => {
     const confirmClose = confirm('프로젝트를 종료하시겠습니까?');
@@ -82,7 +84,7 @@ const IDEHeader: React.FC<IDEHeaderProps> = ({ clientRef }) => {
       </LeftBox>
       <IDEBtnDiv>
         <SmallButton
-          // onClick={handleSave}
+          onClick={handleSave}
           aria-label="save"
           variant="contained"
           size="small"
