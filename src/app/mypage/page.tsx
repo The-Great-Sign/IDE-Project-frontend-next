11b142ce5a05891c5ad0bddc9612fa264d9785fb
nari <naringst@gmail.com>
2023-12-28 04:33:03 +0900

    feat : IDE-148 유저 이름 변경 로직 구현

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import useUserStore from '@/store/useUserStore';
 import MainHeader from '@/components/MainHeader/MainHeader';
@@ -15,16 +15,40 @@ import {
   AccountInfo,
   StyledButton,
   Avatar,
+  NameInput,
 } from './Mypage.styles';
 import useTokenStore from '@/store/useTokenStore';
 import { reloadTokenSetting } from '@/utils/token/reloadTokenSetting';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import axiosInstance from '../api/axiosInstance';
 
 const MyPage = () => {
   const { name, email, imageUrl } = useUserStore.getState();
   const myProjectNum = 3;
   const invitedProjectNum = 3;
+
   const router = useRouter();
+  const [updateUserName, setUpdateUserName] = useState<string>('');
+  const [isEditName, setIsEditName] = useState<boolean>(false);
+
+  const handleUpdateUserName = async (updateUserName: string) => {
+    try {
+      console.log(updateUserName);
+      const response = await axiosInstance.patch(`/user/update/nickname`, {
+        nickname: updateUserName,
+      });
+      // 요청이 성공적으로 처리되었을 때의 로직
+      console.log('Nickname updated successfully:', response.data);
+
+      if (response.data.success) {
+        useUserStore.getState().setNewName(updateUserName);
+      }
+      setIsEditName(false);
+    } catch (error) {
+      // 에러 처리 로직
+      console.error('Error updating nickname:', error);
+    }
+  };
 
   useEffect(() => {
     const storedAccessToken = useTokenStore.getState().accessToken;
@@ -52,8 +76,28 @@ const MyPage = () => {
             />
           </Avatar>
           <UserInfo>
-            <UserName>{name}님</UserName>
-            <StyledButton>이름 변경</StyledButton>
+            {isEditName ? (
+              <>
+                <NameInput
+                  value={updateUserName}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setUpdateUserName(e.target.value);
+                  }}
+                />
+                <StyledButton
+                  onClick={() => handleUpdateUserName(updateUserName)}
+                >
+                  수정 완료
+                </StyledButton>
+              </>
+            ) : (
+              <>
+                <UserName>{name} 님</UserName>
+                <StyledButton onClick={() => setIsEditName(true)}>
+                  이름 변경
+                </StyledButton>
+              </>
+            )}
           </UserInfo>
           <Stats>
             <AccountInfo>생성한 프로젝트 수 : {myProjectNum}개 </AccountInfo>
