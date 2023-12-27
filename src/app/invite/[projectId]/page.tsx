ce939756613a5041c20f4cdbcc63353c334b6ba4
moonjw0 <jjwon1230@naver.com>
2023-12-27 22:37:34 +0900

    feat : IDE-29 코드 실행버튼 추가, axios 변경

--- src/app/invite/[projectId]/page.tsx
+++ src/app/invite/[projectId]/page.tsx
@@ -34,14 +34,7 @@ const Invite = () => {
       const response = await axios.get(
         `${
           process.env.NEXT_PUBLIC_BACKEND_URI
-        }/api/projects/${getCurrentProjectId()}`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            Authorization: localStorage.getItem('accessToken'),
-          },
-        }
+        }/api/projects/${getCurrentProjectId()}`
       );
       const data = response.data;
       if (data.success) {
@@ -56,7 +49,10 @@ const Invite = () => {
 
   const postEnterProject = async (enterData: EnterProps) => {
     try {
-      const response = await axiosInstance.post('/api/projects', enterData);
+      const response = await axiosInstance.post(
+        '/api/projects/join',
+        enterData
+      );
       const data = response.data;
       if (data.success) {
         console.log(data.message);
@@ -123,154 +119,3 @@ const Invite = () => {
 };
 
 export default Invite;
-
-// 'use client';
-// import React, { useEffect } from 'react';
-// import {
-//   IDETitle,
-//   InviteContainer,
-//   InviteEnterButton,
-//   InviteForm,
-//   InvitePage,
-//   InvitePasswordInput,
-//   InvitePasswordTitle,
-//   InviteTitle,
-// } from './Invite.styles';
-// import { getCurrentProjectId } from '@/app/ide/[projectId]/page';
-// import axiosInstance from '@/app/api/axiosInstance';
-// import { useRouter } from 'next/navigation';
-// import useProjectStore from '@/store/useProjectStore';
-// import useUserStore from '@/store/useUserStore';
-
-// interface EnterProps {
-//   password: string;
-//   projectId: string;
-// }
-
-// const Invite = () => {
-//   const router = useRouter();
-//   const [password, setPassword] = React.useState<string>('');
-
-//   const postProjectName = async () => {
-//     try {
-//       const response = await axios.get(
-//         `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/projects/${getCurrentProjectId()}`,
-//         {},
-//         {
-//           headers: {
-//             'Content-Type': 'application/json',
-//             'Access-Control-Allow-Origin': '*',
-//             Authorization: localStorage.getItem('accessToken'),
-//           },
-//         }
-//       );
-//       const data = response.data;
-//       if (data.success) {
-//         useAIChatStore.getState().addAIMessage(data);
-//       } else {
-//         alert(data.message);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   const postEnterProject = async (projectId: string) => {
-//     try {
-//       console.log(localStorage.getItem('accessToken'));
-//       const response = await axios.post(
-//         `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/projects/${projectId}/run`,
-//         {},
-
-//         {
-//           headers: {
-//             'Content-Type': 'application/json',
-//             'Access-Control-Allow-Origin': '*',
-//             Authorization: localStorage.getItem('accessToken'),
-//           },
-//         }
-//       );
-//       console.log(localStorage.getItem('accessToken'));
-//       const data = response.data;
-//       setExecute(data.results);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   const postEnterProject = async (enterData: EnterProps) => {
-//     try {
-//       const response = await axiosInstance.post('/api/projects', enterData);
-//       const data = response.data;
-//       if (data.success) {
-//         console.log(data.message);
-//         router.push(`/ide/${getCurrentProjectId()}`);
-//       } else {
-//         alert(data.message);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   const postEnterProject = async (projectId: string) => {
-//     try {
-//       console.log(localStorage.getItem('accessToken'));
-//       const response = await axios.post(
-//         `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/projects/${projectId}/run`,
-//         {},
-
-//         {
-//           headers: {
-//             'Content-Type': 'application/json',
-//             'Access-Control-Allow-Origin': '*',
-//             Authorization: localStorage.getItem('accessToken'),
-//           },
-//         }
-//       );
-//       console.log(localStorage.getItem('accessToken'));
-//       const data = response.data;
-//       setExecute(data.results);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setPassword(e.target.value);
-//   };
-//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     const enterData = {
-//       password: password,
-//       projectId: getCurrentProjectId(),
-//     };
-//     postEnterProject(enterData);
-//   };
-
-//   useEffect(() => {
-//     if (useUserStore.getState().isLoggedIn == false) {
-//       useProjectStore.getState().setInvitedProjectId(getCurrentProjectId());
-//       router.push('/login');
-//     }
-//     else{
-//       postProjectName();
-//     }
-//   }, []);
-
-//   return (
-//     <InvitePage>
-//       <InviteContainer>
-//         <IDETitle>DJIDE</IDETitle>
-//         <InviteTitle>닉네임님의 프로젝트명 초대 링크</InviteTitle>
-//         <InvitePasswordTitle>비밀번호</InvitePasswordTitle>
-//         <InviteForm onSubmit={handleSubmit}>
-//           <InvitePasswordInput type="password" onChange={handleInput} />
-//           <InviteEnterButton type="submit">입장하기</InviteEnterButton>
-//         </InviteForm>
-//       </InviteContainer>
-//     </InvitePage>
-//   );
-// };
-
-// export default Invite;
