f7bfa0fcdd21b700889741f3db337710a06108a9
rachel <rachel.uiux@gmail.com>
2024-01-02 00:12:46 +0900

    style : IDE-115 메인 페이지 UI 발전, 반응형 작업
    - 메인 이미지 추가(svg)
    - 메인 아이콘 추가
    - InfoBox styles -> page.styles 경로 이동
    - 메인 헤더 호버 text underline 추가

--- src/components/MainHeader/MainHeader.style.ts
+++ src/components/MainHeader/MainHeader.style.ts
@@ -1,3 +1,4 @@
+import { FONTS_WEIGHT } from './../../constants/fonts';
 import { FONTS } from '@/constants/fonts';
 import styled from 'styled-components';
 import Link from 'next/link';
@@ -10,6 +11,7 @@ export const HeaderContainer = styled.div`
   height: 68px;
   background: ${props => props.theme.colors.headerBg};
   z-index: 2;
+  font-weight: ${FONTS_WEIGHT.medium};
 `;
 
 export const Logo = styled.div`
